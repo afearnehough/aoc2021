e08ea7a5f8e2ba3e04ee5ac62f9c00ef94a93159
afearnehough <=>
2021-12-06 12:17:53 +0000

    day6 update

--- day6/day6.py
+++ day6/day6.py
@@ -1,17 +1,17 @@
-days = 80
-age_limit = 8
 input = list(map(int, open("input.txt").read().split(",")))
-fishes = list(map(lambda age: input.count(age), range(age_limit+1)))
 
-print(fishes)
+def simulate(days):
+    age_limit = 8
+    fishes = list(map(lambda age: input.count(age), range(age_limit+1)))
+    for day in range(days):
+        fishes_0 = fishes[0]
+        fishes[0] = 0
+        for age in range(1, (age_limit+1)):
+            fishes[age-1] += fishes[age]
+            fishes[age] = 0
+        fishes[8] += fishes_0
+        fishes[6] += fishes_0
+    return sum(fishes)
 
-for day in range(days):
-    fishes_0 = fishes[0]
-    fishes[0] = 0
-    for age in range(1, (age_limit+1)):
-        fishes[age-1] += fishes[age]
-        fishes[age] = 0
-    fishes[8] += fishes_0
-    fishes[6] += fishes_0
-
-print(sum(fishes))
+print("Part 1: %d" % simulate(80))
+print("Part 2: %d" % simulate(256))
