dd271ef3a6b71673212a8ee6f20c354fe0d8bc4e
Adam Fearnehough <afearnehough@gmail.com>
2021-12-08 17:45:02 +0000

    drawing improvement

--- day8/day8.py
+++ day8/day8.py
@@ -9,16 +9,18 @@ class Display:
         self.digit_count = digit_count
         self.width = (self.digit_width*self.digit_count+digit_count)
         self.pixels = (([' ']*(self.width*self.digit_height)))
-        self.digit = [0,1,1,1,1,0,2,0,0,0,0,3,2,0,0,0,0,3,0,4,4,4,4,0,5,0,0,0,0,6,5,0,0,0,0,6,0,7,7,7,7,0]
+        self.digit_image = [0,1,1,1,1,0,2,0,0,0,0,3,2,0,0,0,0,3,0,4,4,4,4,0,5,0,0,0,0,6,5,0,0,0,0,6,0,7,7,7,7,0]
         self.sigmap = "abcdefg"
 
     def set_digit(self, digit_index, signal):
-        if 0 >= digit_index < self.digit_count:
+        if 0 <= digit_index < self.digit_count:
             for y in range(self.digit_height):
                 for x in range(self.digit_width):
-                    value = (self.digit[x+y*self.digit_width])
+                    value = (self.digit_image[x+y*self.digit_width])
                     pixel = ((digit_index*(self.digit_width+1)+x)+y*self.width)
-                    self.pixels[pixel] = self.sigmap[value-1] if value > 0 and self.sigmap[value-1] in signal else ' '
+                    self.pixels[pixel] = ' '
+                    if value > 0:
+                        self.pixels[pixel] = self.sigmap[value-1] if self.sigmap[value-1] in signal else '.'
 
     def display(self):
         for y in range(self.digit_height):
@@ -28,9 +30,9 @@ class Display:
 decoder = Decoder()
 display = Display()
 display.set_digit(0, "abcdefg")
-display.set_digit(1, "abcdefg")
+display.set_digit(1, "aefg")
 display.set_digit(2, "abcdefg")
-display.set_digit(3, "abcdefg")
+display.set_digit(3, "abcdg")
 display.display()
 
 # display.process_signal_patterns("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb".split())
