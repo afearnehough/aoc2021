25734ba4944163c5989a2ec87739bb9d4f432a8b
afearnehough <=>
2021-12-09 09:09:24 +0000

    Minor

--- day8/day8.py
+++ day8/day8.py
@@ -9,14 +9,11 @@ def str_common(string1, string2):
 class Decoder:
     def __init__(self, patterns):
         self.patterns = patterns
-
         self.mapping = ([None]*10)
-        self.numbers = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
-
-        self.mapping[1] = str_sort(self.get_patterns_of_length(2)[0])
-        self.mapping[7] = str_sort(self.get_patterns_of_length(3)[0])
-        self.mapping[4] = str_sort(self.get_patterns_of_length(4)[0])
-        self.mapping[8] = str_sort(self.get_patterns_of_length(7)[0])
+        self.mapping[1] = str_sort(list(filter(lambda pattern: len(pattern) == 2, patterns))[0])
+        self.mapping[7] = str_sort(list(filter(lambda pattern: len(pattern) == 3, patterns))[0])
+        self.mapping[4] = str_sort(list(filter(lambda pattern: len(pattern) == 4, patterns))[0])
+        self.mapping[8] = str_sort(list(filter(lambda pattern: len(pattern) == 7, patterns))[0])
 
         for pattern in [str_sort(pattern) for pattern in patterns]:
             length = len(pattern)
@@ -39,10 +36,6 @@ class Decoder:
 
     def decode(self, values):
         return [self.mapping.index(value) for value in [str_sort(value) for value in values]]
-        
-    def get_patterns_of_length(self, length):
-        return list(filter(lambda pattern: len(pattern) == length, self.patterns))
-
 
 uniques = 0
 total = 0
