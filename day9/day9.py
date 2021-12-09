1529f1f9c8a17533ab242b28daec1bf65d5fea8e
Adam Fearnehough <afearnehough@gmail.com>
2021-12-09 07:47:19 +0000

    day9 done

---
+++ day9/day9.py
@@ -0,0 +1,34 @@
+
+data = list(map(lambda heights: [int(height) for height in heights], open("input.txt", "r").read().splitlines()))
+size = (len(data[0]), len(data))
+
+def cell(x, y, default=9):
+    return data[y][x] if 0 <= x < size[0] and 0 <= y < size[1] else default
+
+risk_sum = 0
+basins = []
+cells_visited = {}
+
+for y in range(size[1]):
+    for x in range(size[0]):
+        if all(cell(x, y) < height for height in [cell((x+1), y), cell((x-1), y), cell(x, (y-1)), cell(x, (y+1))]):
+            risk_sum += (cell(x, y)+1)
+            basin_size = 0
+            basin_search_stack = [(x, y)]
+
+            while len(basin_search_stack):
+                (sx, sy) = basin_search_stack.pop()
+                cell_index  = (sx+sy*size[0])
+                cell_height = cell(sx, sy)
+
+                if cell_height < 9 and cell_index not in cells_visited:
+                    cells_visited[cell_index] = True
+                    basin_size += 1
+                    basin_search_stack += [((sx+1), sy), ((sx-1), sy), (sx, (sy+1)), (sx, (sy-1))]
+
+            basins.append(basin_size)
+
+print(risk_sum)
+print(basins)
+basins = sorted(basins)[-3:]
+print(basins[0]*basins[1]*basins[2])
