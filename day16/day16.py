ad7de3cddc52019cb6cd81a473c6be93fe8b46ec
Adam Fearnehough <afearnehough@gmail.com>
2021-12-16 12:39:54 +0000

    latest

--- day16/day16.py
+++ day16/day16.py
@@ -1,17 +1,17 @@
-
-
 class Buffer:
     def __init__(self, hex_string):
         self.bit_offset = 0
         self.bytes = bytes.fromhex((hex_string+'0') if (len(hex_string)%2) else hex_string)
+        self.length = len(self.bytes)
 
-    def read(self, bit_count):
-        return self.read_bits(bit_count)
-
-    def read_bits(self, bit_count):
-        for bit in range(self.bit_offset, (self.bit_offset+bit_count)):
-            yield ((self.bytes[bit>>3]>>(7-(bit&7)))&1)
-        self.bit_offset += bit_count
+    def read_bits(self, count):
+        result, result_offset = 0, 0
+        for bit in range(self.bit_offset, (self.bit_offset+count)):
+            if ((self.bytes[bit>>3]>>((7-bit)&7))&1):
+                result |= (1<<(count-1-result_offset))
+            result_offset += 1
+        self.bit_offset += count
+        return result
 
 class BITS:
     def __init__(self):
@@ -19,7 +19,12 @@ class BITS:
 
     def process_transmission(self, transmission):
         buffer = Buffer(transmission)
-        print("".join(map(str, buffer.read(len(transmission)*4))))
+
+
+        print(buffer.read_bits(3))
+        print(buffer.read_bits(3))
+        print(buffer.read_bits(1))
+        print(buffer.read_bits(15))
 
 BITS().process_transmission(open("input.txt").read())
 
