c949c9171e026395dd8a6d3428ad8930a98b7e1a
Adam Fearnehough <afearnehough@gmail.com>
2021-12-16 18:39:18 +0000

    Part 1 done

--- day16/day16.py
+++ day16/day16.py
@@ -1,7 +1,7 @@
 class Buffer:
     def __init__(self, hex_string):
         self.bit_offset = 0
-        self.bytes = bytes.fromhex((hex_string+'0') if (len(hex_string)%2) else hex_string)
+        self.bytes = bytes.fromhex(hex_string)
         self.length = len(self.bytes)
 
     def read_bits(self, count):
@@ -20,7 +20,6 @@ class System:
     def evalulate_packet(self, buffer):
         version = buffer.read_bits(3)
         type_id = buffer.read_bits(3)
-        print("Packet header: %d, %d" % (version, type_id))
         self.version_sum += version
 
         if type_id == 4: # literal
@@ -29,24 +28,33 @@ class System:
             while reading_value_groups:
                 reading_value_groups = buffer.read_bits(1)
                 value = ((value<<4)+buffer.read_bits(4))
-            print("Literal value: %d" % value)
             return value
         else: # operator
-            op_args = []
+            arguments = []
             length_type_id = buffer.read_bits(1)
-            if length_type_id == 0:
-                packet_start_offset = buffer.bit_offset
-                bits_remaining = buffer.read_bits(15)
-                while bits_remaining > 0:
-                    op_args.append(self.evalulate_packet(buffer))
-                    bits_remaining -= (buffer.bit_offset-packet_start_offset)
-            else:
+            if length_type_id == 1:
                 packet_count = buffer.read_bits(11)
-                op_args += [self.evalulate_packet(buffer) for i in range(packet_count)]
-
-        return 0
+                arguments += [self.evalulate_packet(buffer) for i in range(packet_count)]
+            else:
+                bits_remaining = buffer.read_bits(15)
+                packet_start_offset = buffer.bit_offset
+                while bits_remaining > 0:
+                    arguments.append(self.evalulate_packet(buffer))
+                    bits_remaining -= (buffer.bit_offset-packet_start_offset)
+            return 0
 
 system = System()
 result = system.evalulate_packet(Buffer(open("input.txt").read()))
 print("Part 1: %d" % system.version_sum)
 print("Part 2: %d" % result)
+
+# 01100010000000001000000000000000000101100001000101010110001011001000100000000010000100011000111000110100
+
+# 011 000 1 00000000010 | 000 000 0 000000000010110 | 000 100 0 1010 | 101 100 0 1011 | 001 000 1 00000000010 | 000 100 0 1100 | 011 100 0 1101 | 00
+# VVV TTT I LLLLLLLLLLL | VVV TTT I LLLLLLLLLLLLLLL | VVV TTT G NNNN | VVV TTT G NNNN | VVV TTT I LLLLLLLLLLL | VVV TTT G NNNN | VVV TTT G NNNN |
+#   3   0 1           2 |   0   0 0              22 |   0   4 0   10 |   5   4 0   11 |   1   0 1           2 |   0   4 0   12 |   3   4 0   13 |
+
+# remaining: 20
+# start: 5
+# current 10
+# remaining: 20-(10-5)
