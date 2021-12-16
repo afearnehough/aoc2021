22662fad05ec01524c860013e268361ee854e617
Adam Fearnehough <afearnehough@gmail.com>
2021-12-16 19:48:03 +0000

    Day16 done

--- day16/day16.py
+++ day16/day16.py
@@ -1,3 +1,6 @@
+import operator as op
+from functools import reduce
+
 class Buffer:
     def __init__(self, hex_string):
         self.bit_offset = 0
@@ -16,6 +19,15 @@ class Buffer:
 class System:
     def __init__(self):
         self.version_sum = 0
+        self.ops = {
+            0: lambda args: reduce(lambda x, y: (x+y), args),
+            1: lambda args: reduce(lambda x, y: (x*y), args),
+            2: min,
+            3: max,
+            5: lambda args: 1 if args[0]  > args[1] else 0,
+            6: lambda args: 1 if args[0]  < args[1] else 0,
+            7: lambda args: 1 if args[0] == args[1] else 0
+        }
 
     def evalulate_packet(self, buffer):
         version = buffer.read_bits(3)
@@ -36,25 +48,13 @@ class System:
                 packet_count = buffer.read_bits(11)
                 arguments += [self.evalulate_packet(buffer) for i in range(packet_count)]
             else:
-                bits_remaining = buffer.read_bits(15)
+                bit_length = buffer.read_bits(15)
                 packet_start_offset = buffer.bit_offset
-                while bits_remaining > 0:
+                while (buffer.bit_offset-packet_start_offset) < bit_length:
                     arguments.append(self.evalulate_packet(buffer))
-                    bits_remaining -= (buffer.bit_offset-packet_start_offset)
-            return 0
+            return self.ops[type_id](arguments)
 
 system = System()
 result = system.evalulate_packet(Buffer(open("input.txt").read()))
 print("Part 1: %d" % system.version_sum)
 print("Part 2: %d" % result)
-
-# 01100010000000001000000000000000000101100001000101010110001011001000100000000010000100011000111000110100
-
-# 011 000 1 00000000010 | 000 000 0 000000000010110 | 000 100 0 1010 | 101 100 0 1011 | 001 000 1 00000000010 | 000 100 0 1100 | 011 100 0 1101 | 00
-# VVV TTT I LLLLLLLLLLL | VVV TTT I LLLLLLLLLLLLLLL | VVV TTT G NNNN | VVV TTT G NNNN | VVV TTT I LLLLLLLLLLL | VVV TTT G NNNN | VVV TTT G NNNN |
-#   3   0 1           2 |   0   0 0              22 |   0   4 0   10 |   5   4 0   11 |   1   0 1           2 |   0   4 0   12 |   3   4 0   13 |
-
-# remaining: 20
-# start: 5
-# current 10
-# remaining: 20-(10-5)
